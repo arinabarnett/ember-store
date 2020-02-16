d8eb2b5cb53306dabf092890b85c40baf3ec8675
Arina <arishilyaeva@gmail.com>
2020-02-16 14:18:39 +0300

    Remove api folder

--- store-api/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http'); // Import http package
-const app = require('./app'); // Import app.js file from current directory
-
-const port = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-server.listen(port);
